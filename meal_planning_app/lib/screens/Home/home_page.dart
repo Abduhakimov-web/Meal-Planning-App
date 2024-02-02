c90b757b4f4cec139bb1a1271e715a374443983c
MuslimaAkbarova <2110183@newuzbekistanuniversity.uz>
2024-02-02 13:49:29 +0500

    home_page

--- meal_planning_app/lib/screens/Home/home_page.dart
+++ meal_planning_app/lib/screens/Home/home_page.dart
@@ -7,13 +7,14 @@ import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+  
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   ScrollController _scrollController = ScrollController();
-  
+
   @override
   void initState() {
     _scrollController.addListener(() {
